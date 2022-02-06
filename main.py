ba8b634744fe0eed31ad3f381562dd0a9c7c1006
danilevich23555 <danilevich23555@yandex.ru>
2022-02-06 15:25:43 +0300

    update hw

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import datetime
 
 def log_decorator(function):
-
     def new_fanction(*args, **kwargs):
         now = datetime.datetime.now()
         name_file = 'log-file' + f'{now.strftime("%d-%m-%Y-%H-%M-%S")}' + '.txt'
@@ -12,4 +11,30 @@ def log_decorator(function):
         path = os.path.os.getcwd()
         with open(os.path.join(os.getcwd(), name_file), 'w') as log:
             log.write(f'{now.strftime("%d-%m-%Y %H:%M:%S")}'+'|'+f'{function.__name__}'+' | '+f'аргументы {args, kwargs}'+' | '+f'с результатом {result}'+' | '+f'{path}')
+        return result
     return new_fanction
+
+
+def parametrized_decor(parameter):
+    def log_decorator1(function):
+        def new_fanction(*args, **kwargs):
+            now = datetime.datetime.now()
+            name_file = 'log-file' + f'{now.strftime("%d-%m-%Y-%H-%M-%S")}' + '.txt'
+            my_file = open(name_file, "w+")
+            my_file.close()
+            result = function(*args, **kwargs)
+            path = parameter
+            with open(os.path.join(path, name_file), 'w') as log:
+                log.write(
+                    f'{now.strftime("%d-%m-%Y %H:%M:%S")}' + '|' + f'{function.__name__}' + ' | ' + f'аргументы {args, kwargs}' + ' | ' + f'с результатом {result}' + ' | ' + f'{path}')
+            return result
+        return new_fanction
+    return log_decorator1
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-from main import log_decorator
+from main import log_decorator, parametrized_decor
 
 
 
 
 
 @log_decorator
-def foo(a, b):
+def foo1(a, b):
     c = a + b
     return c
 
+three = foo1(1, 2)
+five = foo1(2, 3)
+
+result = foo1(three, five)
 
 
-foo(4, 7)
-foo(4, 5)
-foo(4, 8)
+
+
+path1 = 'D:\\task_one_test\\проффесиональная работа с Python\\домашние задание к 5 лекции\\log'
+
+foo1(4,5)
+
+
+
+@parametrized_decor(parameter = path1)
+def foo():
+ pass
+
+foo()
